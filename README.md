170e4d00b7b9d90105cf54412ceead10d73f7505
Jesus Orosco <jorosco@salesforce.com>
2022-05-07 07:27:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# aws-adder
+Add aws credentials to your credentials file from your Clipboard
+
+## Ubuntu Focal dependencies
+```shell
+apt-get install libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev
+```
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use log::{debug, error, log_enabled, info, Level};
+use std::env;
+
+use clap::Parser;
+use clipboard::ClipboardProvider;
+use clipboard::ClipboardContext;
+use configparser::ini::Ini;
+
+#[derive(Parser)]
+#[clap(about, author, version, long_about = None)]
+struct Args {
+    // Location of credentials file
+    #[clap(short, long, default_value_t = String::from(
+        format!("{}/.aws/credentials", env::var("HOME").expect("$HOME is not set")))
+    )]
+    file_name: String,
+
+    #[clap(short, long, default_value = "default")]
+    profile: String,
+}
+
+fn main() {
+    env_logger::init();
+    let args = Args::parse();
+
+    let mut config = Ini::new();
+    info!("Opening {}", &args.file_name);
+    config.load(&args.file_name).unwrap();
+
+    let clippy: String = read_clipboard();
+    let splits = clippy.trim().split_ascii_whitespace();
+
+    for s in splits {
+        if s.find("AWS") != None {
+            let e = s.split_once("=").unwrap();
+            let lower = e.0.to_lowercase();
+            let key = lower.as_str();
+            let value: Option<String> = Some(String::from(e.1));
+            config.set(&args.profile, key, value);
+        }
+    }
+    config.write(&args.file_name).unwrap();
+    info!("profile: {} added to: {}", &args.profile, &args.file_name);
+}
+
+fn read_clipboard() -> String {
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    String::from(ctx.get_contents().unwrap())
+}
+
+//fn first_name(x: &'static str) -> &'static str {
+//    x
+//}
